54989b0c1349d759d97440939c7fdc3bb5302f8b
jeesay <jeanchristophe.taveau@gmail.com>
2022-04-26 10:15:22 +0200

    Create introduction.js

---
+++ src/introduction.js
@@ -0,0 +1,29 @@
+/*
+ *  QUIBI-Game Quest in Bioinformatics Serious Game
+ *  Copyright (C) 2021  Jean-Christophe Taveau.
+ *
+ *  This file is part of QUIBI-Game
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with QUIBI-Game.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Authors:
+ * Jean-Christophe Taveau
+ * Louis Texier
+ * Léa Chabot
+ * Bluwen Guidoux D'Halluin
+ * Fatoumata Mangane
+*/
+
+'use strict';
